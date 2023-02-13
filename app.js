cebed2845c5153c07aafe8235c2ddfc9502c175a
Joominseok <jumseok12@gmail.com>
2023-02-13 21:21:56 +0900

    feat: home login profile signup 페이지 api 끝

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
+
+let corsOptions = {
+  origin: 'http://localhost:5000',
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 app.get("/", (req,res) => {
   res.sendFile(__dirname + "/html/login.html")
@@ -8,6 +16,14 @@ app.get("/", (req,res) => {
 
 app.get("/home", (req,res) => {
   res.sendFile(__dirname + "/html/home.html")
+})
+
+app.get("/signup", (req,res) => {
+  res.sendFile(__dirname + "/html/signup.html")
+})
+
+app.get("/profile", (req,res) => {
+  res.sendFile(__dirname + "/html/profile.html")
 })
 
 app.listen(5100, console.log("프론트 서버 온!"))
